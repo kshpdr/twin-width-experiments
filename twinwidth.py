fc4d4d3c90d888e9c0da55717965da3c28939655
Denis Koshelev <truemacintoshchannel@gmail.com>
2023-06-09 10:46:04 +0200

    Add new heuristics

--- twinwidth.py
+++ twinwidth.py
@@ -3,6 +3,9 @@ import graph_tool.all as gt
 import random
 import sys
 
+import graph_tool.topology
+import numpy as np
+
 
 class Graph:
     def __init__(self):
@@ -12,6 +15,33 @@ class Graph:
         self.graph.properties[("v", "red_edges")] = self.graph.new_vertex_property('int', val=False)
         self.graph.properties[("e", "red_edges")] = self.graph.new_edge_property('bool', val=False)
         self.twin_width = 0
+        self.merge_costs = {}
+
+    def set_merge_cost(self, v, u, cost):
+        self.merge_costs[(v, u)] = cost
+
+    def get_merge_cost(self, v, u):
+        return self.merge_costs.get((v, u), None)
+
+    def update_merge_scores(self, vertices):
+        for v in vertices:
+            v = self.get_vertex_by_id(v)
+            for neighbor in self.get_vertices():
+                if v == neighbor: continue
+                # neighbor = self.get_vertex(neighbor)
+                temp_graph = self.graph.copy()
+                temp_input_graph = Graph()
+                temp_input_graph.graph = temp_graph
+                temp_input_graph.merge_vertices(v, neighbor)
+
+                input_graph.set_merge_cost(input_graph.get_vertex_id(v), input_graph.get_vertex_id(neighbor), max(temp_graph.vp["red_edges"].a))
+                input_graph.set_merge_cost(input_graph.get_vertex_id(neighbor), input_graph.get_vertex_id(v), max(temp_graph.vp["red_edges"].a))
+
+    def print_edges(self):
+        print([f"{e.source()} {e.target()}" for e in self.get_edges()])
+
+    def print_vp(self, name):
+        print(list(self.graph.vp[name]))
 
     def get_vertex(self, i):
         return self.graph.vertex(i)
@@ -123,11 +153,18 @@ class Graph:
         self.mark_vertex_neighbors_red(source, twin_unique_neighbors)
 
         # remove merged vertex
+        self.delete_from_merge_costs(twin)
         self.remove_vertex(twin)
         # print([f"{input_graph.get_vertex_id(e.source())} {input_graph.get_vertex_id(e.target())}: {input_graph.get_red_edges()[e]}" for e in input_graph.get_edges()])
         # print([f"{input_graph.get_vertex_id(v)}: {input_graph.get_red_edges_pro_vertex()[v]}" for v in self.get_vertices()])
         self.twin_width = max(self.twin_width, self.get_red_edges_amount())
 
+    def delete_from_merge_costs(self, to_delete):
+        for v in self.get_vertices():
+            if (input_graph.get_vertex_id(v), input_graph.get_vertex_id(to_delete)) in self.merge_costs:
+                del self.merge_costs[(input_graph.get_vertex_id(v), input_graph.get_vertex_id(to_delete))]
+            if (input_graph.get_vertex_id(to_delete), input_graph.get_vertex_id(v)) in self.merge_costs:
+                del self.merge_costs[(input_graph.get_vertex_id(to_delete), input_graph.get_vertex_id(v))]
 
 def find_random_contraction_sequence(input_graph):
     current_sequence = ""
@@ -144,20 +181,166 @@ def find_random_contraction_sequence(input_graph):
     return current_sequence
 
 
+def find_table_contraction_sequence(input_graph):
+    current_sequence = ""
+
+    while input_graph.graph.num_vertices() > 1:
+        best_pair = ()
+        best_score = float("inf")
+        for v in input_graph.get_vertices():
+            for u in input_graph.get_vertices():
+                if v == u: continue
+
+                temp_graph = input_graph.graph.copy()
+
+                # Merging vertices in the temporary graph
+                temp_input_graph = Graph()
+                temp_input_graph.graph = temp_graph
+                temp_input_graph.merge_vertices(v, u)
+
+                # Calculating the sum of red edges after contraction
+                red_sum = max(temp_graph.vp["red_edges"].a)
+
+                # Updating minimum score and best pair if needed
+                if red_sum < best_score:
+                    best_score = red_sum
+                    best_pair = (v, u)
+
+                # print(int(v) + 1, int(u) + 1)
+                # temp_input_graph.print_vp("red_edges")
+
+        v, u = best_pair[0], best_pair[1]
+        current_sequence += f"{input_graph.get_vertex_id(v)} {input_graph.get_vertex_id(u)} \n"
+        # print(f"Found ({best_score}): ", input_graph.get_vertex_id(v), input_graph.get_vertex_id(u))
+        input_graph.merge_vertices(v, u)
+        # input_graph.print_vp("red_edges")
+
+    current_sequence += f"c twin width: {input_graph.get_twin_width()}"
+
+    return current_sequence
+
+
+def find_table_set_contraction_sequence(input_graph):
+    current_sequence = ""
+
+    for v in input_graph.get_vertices():
+        for u in input_graph.get_vertices():
+            if v == u: continue
+
+            temp_graph = input_graph.graph.copy()
+
+            # Merging vertices in the temporary graph
+            temp_input_graph = Graph()
+            temp_input_graph.graph = temp_graph
+            temp_input_graph.merge_vertices(v, u)
+
+            input_graph.set_merge_cost(input_graph.get_vertex_id(v), input_graph.get_vertex_id(u), max(temp_graph.vp["red_edges"].a))
+            input_graph.set_merge_cost(input_graph.get_vertex_id(u), input_graph.get_vertex_id(v), max(temp_graph.vp["red_edges"].a))
+
+    while input_graph.graph.num_vertices() > 1:
+        (v, u) = min(input_graph.merge_costs, key=input_graph.merge_costs.get)
+        (v, u) = input_graph.get_vertex_by_id(v), input_graph.get_vertex_by_id(u)
+
+        to_update_neighbors = set(input_graph.get_neighbors(v)).union(set(input_graph.get_neighbors(u))).union({int(v)})
+        if u in to_update_neighbors: to_update_neighbors.remove(u)
+
+        to_update_neighbors = [input_graph.get_vertex_id(v) for v in to_update_neighbors]
+
+        current_sequence += f"{input_graph.get_vertex_id(v)} {input_graph.get_vertex_id(u)} \n"
+        # print(f"Found ({input_graph.get_merge_cost(input_graph.get_vertex_id(v), input_graph.get_vertex_id(u))}): ", input_graph.get_vertex_id(v), input_graph.get_vertex_id(u))
+        input_graph.merge_vertices(v, u)
+        input_graph.update_merge_scores(to_update_neighbors)
+
+    current_sequence += f"c twin width: {input_graph.get_twin_width()}"
+
+    return current_sequence
+
+
+def find_vc_similarity_contraction_sequence(input_graph):
+    current_sequence = ""
+
+    while input_graph.graph.num_vertices() > 1:
+        best_pair = ()
+        best_score = float("-inf")
+        for v in input_graph.get_vertices():
+            for u in input_graph.get_vertices():
+                if v == u: continue
+                if input_graph.graph.num_vertices() == 2:
+                    best_pair = (input_graph.graph.vertex(0), input_graph.graph.vertex(1))
+
+                temp_graph = input_graph.graph.copy()
+
+                # Merging vertices in the temporary graph
+                temp_input_graph = Graph()
+                temp_input_graph.graph = temp_graph
+                temp_input_graph.merge_vertices(v, u)
+
+                # Calculating the sum of red edges after contraction
+                red_sum = graph_tool.topology.vertex_similarity(temp_graph, sim_type='leicht-holme-newman', vertex_pairs=[(v, u)])
+
+                # Updating minimum score and best pair if needed
+                if red_sum > best_score:
+                    best_score = red_sum
+                    best_pair = (v, u)
+
+                # print(int(v) + 1, int(u) + 1)
+                # temp_input_graph.print_vp("red_edges")
+
+        v, u = best_pair[0], best_pair[1]
+        current_sequence += f"{input_graph.get_vertex_id(v)} {input_graph.get_vertex_id(u)} \n"
+        print(f"Found ({best_score}): ", input_graph.get_vertex_id(v), input_graph.get_vertex_id(u))
+        input_graph.merge_vertices(v, u)
+        input_graph.print_vp("red_edges")
+
+    current_sequence += f"c twin width: {input_graph.get_twin_width()}"
+
+    return current_sequence
+
+
+def find_from_cliques_contraction_sequence(input_graph):
+    current_sequence = ""
+
+    while input_graph.graph.num_vertices() > 1:
+        cliques = gt.max_cliques(input_graph.graph)
+        clique = next(cliques)
+
+        # print(len(list(input_graph.get_vertices())))
+        while len(clique) > 1:
+            v, u = input_graph.get_vertex(clique[0]), input_graph.get_vertex(clique[1])
+            # print(f"Merged {input_graph.get_vertex_id(v)} {input_graph.get_vertex_id(u)}")
+            clique = [el - 1 if el > clique[1] else el for el in clique]
+            input_graph.merge_vertices(v, u)
+            current_sequence += f"{input_graph.get_vertex_id(clique[0])} {input_graph.get_vertex_id(clique[1])} \n"
+            clique = np.delete(clique, 1)
+
+    current_sequence += f"c twin width: {input_graph.get_twin_width()}"
+    return current_sequence
+
+
 def simulate(input_graph):
     current_sequence = ""
-    pairs = """ """
+    pairs = """2 3
+    2 4
+    10 11
+    10 8
+    10 9
+    6 7
+    2 1
+    2 5
+    2 6
+    2 10"""
 
     for p in pairs.split("\n"):
         p = p.strip().split()
         v, u = input_graph.get_vertex_by_id(int(p[0])), input_graph.get_vertex_by_id(int(p[1]))
         current_sequence += f"{input_graph.get_vertex_id(v)} {input_graph.get_vertex_id(u)} \n"
-        print(f"{input_graph.get_vertex_id(v)} {input_graph.get_vertex_id(u)} \n")
+        # print(f"{input_graph.get_vertex_id(v)} {input_graph.get_vertex_id(u)} \n")
         input_graph.merge_vertices(v, u)
 
     current_sequence += f"c twin width: {input_graph.get_twin_width()}"
 
     return current_sequence
+
 
 def read_input_graph_from_lines(input_lines):
     graph = Graph()
@@ -195,5 +378,10 @@ if __name__ == '__main__':
         input_lines = sys.stdin.readlines()
         input_graph = read_input_graph_from_lines(input_lines)
 
-    best_sequence = find_random_contraction_sequence(input_graph)
-    print(best_sequence)
+    # input_graph.print_edges()
+    # [print(v) for v in gt.vertex_similarity(input_graph.graph)]
+
+    print(find_table_set_contraction_sequence(input_graph))
+
+    # best_sequence = simulate(input_graph)
+    # print(best_sequence)
